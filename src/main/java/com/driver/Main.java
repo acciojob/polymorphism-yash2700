67b8d6614683163983ac963861607504171fd5fe
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2022-12-22 16:44:13 +0530

    1st

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,18 @@ package com.driver;
 
 public class Main {
 
+    public static class Product {
+
+        public int product(int x, int y) {
+            return (x * y);
+        }
+
+        public int product(int x, int y, int z) {
+            return (x * y * z);
+        }
+
+        public double product(double x, double y) {
+            return (x * y);
+        }
+    }
 }

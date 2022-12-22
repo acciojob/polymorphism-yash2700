78ca55f03ec8bc64ca7eb0fdbed2d5a6601d70ce
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2022-12-22 16:47:03 +0530

    3rd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,10 +16,12 @@ public class Main {
             return (x * y);
         }
     }
+    public static void main(String[] args){
     Product obj1=new Product();
     obj1.product(1,2);
     Product obj2=new Product();
     obj.product(1,2,3);
     Product obj3=new Product();
     obj3.product(2.0,3.0);
+    }
 }
